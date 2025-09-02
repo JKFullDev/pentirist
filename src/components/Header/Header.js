7d36b238b2f9c8d3bf6fc44f7c0bd8dda211167d
JKFullDev <jcah009@gmail.com>
2025-09-02 10:52:47 +0200

    cambios;

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,15 @@ const Header = () => {
     <img src="https://seekvectors.com/files/download/pintest-blue-circle-outline-logo-01.png" alt="Pinterest Logo" />
     </div>
     <div class="searchBar">
-    <input id="photo_input" type="text" placeholder="¿Qué estás buscando?" />
+    <input id="photo_input" type="text" placeholder="Search" />
     <select id="color_input">
-      <option value="">Select color</option>
+      <option value="">Color</option>
       <option value="black_and_white">Black and white</option>
       <option value="orange">Orange</option>
       <option value="green">Green</option>
     </select>
+    
+  <input id="imgNum_input" type="number" min="10" max="30" placeholder="  IpP" title="Images per page">
     <button id="search_btn">Search</button>
     </div>
     `;
