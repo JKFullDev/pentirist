609469c646387883e8db2ba30a081396227d66cb
JKFullDev <jcah009@gmail.com>
2025-09-02 12:25:01 +0200

    terminado;

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import "./Header.css";
 
 const Header = () => {
-    const header = document.createElement("header");
-    header.innerHTML = `
+  const header = document.createElement("header");
+  header.innerHTML = `
     <div class="logo">
     <img src="https://seekvectors.com/files/download/pintest-blue-circle-outline-logo-01.png" alt="Pinterest Logo" />
     </div>
+    <button id="open" class="open-menu"><i class="bi bi-list"></i></button>
+    <nav class="nav" id="nav">
+    <button class="close-menu" id="close"><i class="bi bi-x"></i></button>
     <div class="searchBar">
     <input id="photo_input" type="text" placeholder="Search" />
     <select id="color_input">
@@ -18,9 +21,24 @@ const Header = () => {
   <input id="imgNum_input" type="number" min="10" max="30" placeholder="  IpP" title="Images per page">
     <button id="search_btn">Search</button>
     </div>
+    </nav>
     `;
 
-    return header;
+
+  const nav = header.querySelector("#nav");
+  const open = header.querySelector("#open");
+  const close = header.querySelector("#close");
+
+  open.addEventListener("click", () => {
+    nav.classList.add("visible");
+  });
+
+  close.addEventListener("click", () => {
+    nav.classList.remove("visible");
+  })
+
+  return header;
 };
 
-export default Header; 
+export default Header;
+
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,12 +1,11 @@
 import "./Pagination.css";
 
 const Pagination = () => {
-    const footer = document.createElement("footer");
-    footer.innerHTML = `
+  const footer = document.createElement("footer");
+  footer.innerHTML = `
     <button id="prevBtn">Previous</button>
     <button id="nextBtn">Next</button>
   `;
-    return footer;
+  return footer;
 };
-
 export default Pagination;
